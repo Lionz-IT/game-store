c7cef6350f13aabaade55d28bf8469bb9192000d
Rafif Ahmad Yudhistira <raymlg2005@gmail.com>
2026-05-21 23:58:28 +0700

    fix: total csrf bypass and aggressive cache clearing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,6 +7,6 @@ use App\Http\Controllers\GameController;
 Route::get('games', [GameController::class, 'index']);
 Route::get('games/{game}', [GameController::class, 'show']);
 
-Route::post('games', [GameController::class, 'store']);
-Route::put('games/{game}', [GameController::class, 'update']);
-Route::delete('games/{game}', [GameController::class, 'destroy']);
+Route::post('games', [GameController::class, 'store'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::put('games/{game}', [GameController::class, 'update'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::delete('games/{game}', [GameController::class, 'destroy'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
