ed8cad8214d1f545c463d5e822190fef8b23a48d
Rafif Ahmad Yudhistira <raymlg2005@gmail.com>
2026-05-21 19:36:48 +0700

    remove auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\GameController;
 Route::get('games', [GameController::class, 'index']);
 Route::get('games/{game}', [GameController::class, 'show']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('games', [GameController::class, 'store']);
-    Route::put('games/{game}', [GameController::class, 'update']);
-    Route::delete('games/{game}', [GameController::class, 'destroy']);
-});
+Route::post('games', [GameController::class, 'store']);
+Route::put('games/{game}', [GameController::class, 'update']);
+Route::delete('games/{game}', [GameController::class, 'destroy']);
