94ed33d6d69e76ad09e7f82c739d192a16687921
Rafif Ahmad Yudhistira <raymlg2005@gmail.com>
2026-05-22 00:11:25 +0700

    fix: revert post route to use GameController for database storage

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,13 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 
-// TEST RUTE POST PALING SEDERHANA
-Route::post('games', function (Request $request) {
-    return response()->json([
-        'status' => 'success',
-        'data' => $request->all()
-    ], 201);
-});
+// Kembali ke Controller untuk proses database
+Route::post('games', [GameController::class, 'store']);
 
 Route::get('games', [GameController::class, 'index']);
 Route::get('games/{game}', [GameController::class, 'show']);
